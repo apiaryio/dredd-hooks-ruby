40ce212e45fe5cff0b3276586610f2a7254cc315
Adam Kliment <adam@apiary.io>
2015-07-03 16:34:36 +0200

    Workaround for hanging server

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,5 +2,9 @@ require 'aruba/cucumber'
 require "sinatra/base"
 
 Before do
+  puts "Killing server..."
+  system "for i in `ps axu | grep 'server.rb' | grep ruby | awk '{print $2}'`; do kill -9 $i; done > /dev/null 2>&1"
+  sleep 3
+
   @aruba_timeout_seconds = 10
 end
