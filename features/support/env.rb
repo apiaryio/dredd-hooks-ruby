c9c31d9bf79a544772604e2a3466410e5dfdf6ef
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2016-04-24 15:08:22 +1000

    Workaround broken test suite
    The 'bundle exec rake' command did fail with message:
    
        $ bundle exec rake
        Feature: Execution order
    
        Killed: 9
    
    I suspect that the error was shadowed because the Gemfile.lock was missing,
    but I am not sure exactly why. (Travis seems to be using the new
    'features/support/env.rb and still doing fine.)
    
    Anyway, the error is present in my development env. since https://github.com/gonzalo-bulnes/dredd-hooks-ruby/commit/8dc3e2690fa285609ed05f320cb55f3386b3032c
    and appeared in Travis CI when I added the Gemfile.lock.
    
    See https://travis-ci.org/gonzalo-bulnes/dredd-hooks-ruby/jobs/125324433
    
    This commit partially reverts https://github.com/gonzalo-bulnes/dredd-hooks-ruby/commit/8dc3e2690fa285609ed05f320cb55f3386b3032c

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,9 +4,6 @@ require "sinatra/base"
 Before do
   puts "Killing server..."
   system "for i in `ps axu | grep 'server.rb' | grep ruby | awk '{print $2}'`; do kill -9 $i; done > /dev/null 2>&1"
-  puts "Killing handler..."
-  system "for i in `ps axu | grep 'dredd-hooks' | grep ruby | awk '{print $2}'`; do kill -9 $i; done > /dev/null 2>&1"
-  sleep 3
 
   @aruba_timeout_seconds = 10
 end
