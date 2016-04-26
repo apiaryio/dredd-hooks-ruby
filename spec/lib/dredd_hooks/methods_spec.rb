e162a6d8fcbedb5e6da78981eb78e2d9c7ff517a
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2016-04-26 21:17:07 +1000

    Minor improve specs readability

--- spec/lib/dredd_hooks/methods_spec.rb
+++ spec/lib/dredd_hooks/methods_spec.rb
@@ -12,35 +12,35 @@ module DreddHooks
 
     let(:hooks_file) { DummyHooksFile.new }
 
-    it 'is defines #before', public: true do
+    it 'defines #before', public: true do
       expect(hooks_file).to respond_to :before
     end
 
-    it 'is defines #before_validation', public: true do
+    it 'defines #before_validation', public: true do
       expect(hooks_file).to respond_to :before_validation
     end
 
-    it 'is defines #after', public: true do
+    it 'defines #after', public: true do
       expect(hooks_file).to respond_to :after
     end
 
-    it 'is defines #before_each', public: true do
+    it 'defines #before_each', public: true do
       expect(hooks_file).to respond_to :before_each
     end
 
-    it 'is defines #before_each_validation', public: true do
+    it 'defines #before_each_validation', public: true do
       expect(hooks_file).to respond_to :before_each_validation
     end
 
-    it 'is defines #after_each', public: true do
+    it 'defines #after_each', public: true do
       expect(hooks_file).to respond_to :after_each
     end
 
-    it 'is defines #before_all', public: true do
+    it 'defines #before_all', public: true do
       expect(hooks_file).to respond_to :before_all
     end
 
-    it 'is defines #after_all', public: true do
+    it 'defines #after_all', public: true do
       expect(hooks_file).to respond_to :after_all
     end
 
--- spec/lib/dredd_hooks/runner_spec.rb
+++ spec/lib/dredd_hooks/runner_spec.rb
@@ -1,28 +1,33 @@
 require 'spec_helper'
 
 module DreddHooks
-  describe Runner, protected: true do
+  describe Runner do
 
     let(:runner) { Runner.instance }
 
-    it { expect(runner).to respond_to :run_before_hooks_for_transaction }
-    it { expect(runner).to respond_to :run_before_validation_hooks_for_transaction }
-    it { expect(runner).to respond_to :run_after_hooks_for_transaction }
-    it { expect(runner).to respond_to :run_before_each_hooks_for_transaction }
-    it { expect(runner).to respond_to :run_before_each_validation_hooks_for_transaction }
-    it { expect(runner).to respond_to :run_after_each_hooks_for_transaction }
-    it { expect(runner).to respond_to :run_before_all_hooks_for_transaction }
-    it { expect(runner).to respond_to :run_after_all_hooks_for_transaction }
+    describe 'exposes an interface to run hooks', protected: true do
 
-    it { expect(runner).to respond_to :register_before_hook }
-    it { expect(runner).to respond_to :register_before_validation_hook }
-    it { expect(runner).to respond_to :register_after_hook }
-    it { expect(runner).to respond_to :register_before_each_hook }
-    it { expect(runner).to respond_to :register_before_each_validation_hook }
-    it { expect(runner).to respond_to :register_after_each_hook }
-    it { expect(runner).to respond_to :register_before_all_hook }
-    it { expect(runner).to respond_to :register_after_all_hook }
+      it { expect(runner).to respond_to :run_before_hooks_for_transaction }
+      it { expect(runner).to respond_to :run_before_validation_hooks_for_transaction }
+      it { expect(runner).to respond_to :run_after_hooks_for_transaction }
+      it { expect(runner).to respond_to :run_before_each_hooks_for_transaction }
+      it { expect(runner).to respond_to :run_before_each_validation_hooks_for_transaction }
+      it { expect(runner).to respond_to :run_after_each_hooks_for_transaction }
+      it { expect(runner).to respond_to :run_before_all_hooks_for_transaction }
+      it { expect(runner).to respond_to :run_after_all_hooks_for_transaction }
+    end
 
+    describe 'exposes an interface to register hooks', private: true do
+
+      it { expect(runner).to respond_to :register_before_hook }
+      it { expect(runner).to respond_to :register_before_validation_hook }
+      it { expect(runner).to respond_to :register_after_hook }
+      it { expect(runner).to respond_to :register_before_each_hook }
+      it { expect(runner).to respond_to :register_before_each_validation_hook }
+      it { expect(runner).to respond_to :register_after_each_hook }
+      it { expect(runner).to respond_to :register_before_all_hook }
+      it { expect(runner).to respond_to :register_after_all_hook }
+    end
   end
 end
 
