44772e3c0c4f43e29b0117068326c40d5121f77e
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2016-04-27 22:49:37 +1000

    Refactor add generator of runners for multip. transactions hooks

--- lib/dredd_hooks/runner.rb
+++ lib/dredd_hooks/runner.rb
@@ -33,8 +33,25 @@ module DreddHooks
     end
     private_class_method :define_hooks_on_multiple_transactions
 
+    def self.define_runners_for_hooks_on_multiple_transactions
+      HOOKS_ON_MULTIPLE_TRANSACTIONS.each do |hook_name|
+
+        define_method "run_#{hook_name}_hooks_for_transaction" do |transaction|
+          hooks = instance_variable_get("@#{hook_name}_hooks") || []
+          hooks.each do |hook|
+            hook.call(transaction)
+          end
+          return transaction
+        end
+
+      end
+    end
+    private_class_method :define_runners_for_hooks_on_multiple_transactions
+
     define_hooks_on_single_transactions
     define_hooks_on_multiple_transactions
+
+    define_runners_for_hooks_on_multiple_transactions
 
     #
     # Runers for Transaction specific hooks
@@ -65,54 +82,6 @@ module DreddHooks
       transaction_name = transaction["name"]
       transaction_hooks = hooks.fetch(transaction_name, [])
       transaction_hooks.each do |hook_proc|
-        hook_proc.call(transaction)
-      end
-      return transaction
-    end
-
-    #
-    # Runners for *_each hooks API
-    #
-
-    def run_before_each_hooks_for_transaction(transaction)
-      hooks = @before_each_hooks || []
-      hooks.each do |hook_proc|
-        hook_proc.call(transaction)
-      end
-      return transaction
-    end
-
-    def run_before_each_validation_hooks_for_transaction(transaction)
-      hooks = @before_each_validation_hooks || []
-      hooks.each do |hook_proc|
-        hook_proc.call(transaction)
-      end
-      return transaction
-    end
-
-    def run_after_each_hooks_for_transaction(transaction)
-      hooks = @after_each_hooks || []
-      hooks.each do |hook_proc|
-        hook_proc.call(transaction)
-      end
-      return transaction
-    end
-
-    #
-    # Runners for *_all hooks API
-    #
-
-    def run_before_all_hooks_for_transaction(transaction)
-      hooks = @before_all_hooks || []
-      hooks.each do |hook_proc|
-        hook_proc.call(transaction)
-      end
-      return transaction
-    end
-
-    def run_after_all_hooks_for_transaction(transaction)
-      hooks = @after_all_hooks || []
-      hooks.each do |hook_proc|
         hook_proc.call(transaction)
       end
       return transaction
