4e3aa9c6d9467c5ba58ca85af53d88d8ceb5584c
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2016-04-27 23:38:59 +1000

    Refactor add generator of hooks on single transactions

--- lib/dredd_hooks/methods.rb
+++ lib/dredd_hooks/methods.rb
@@ -17,27 +17,19 @@ module DreddHooks
     end
     private_class_method :define_hooks_on_single_transactions
 
+    def self.define_hooks_on_multiple_transactions
+      HOOKS_ON_MULTIPLE_TRANSACTIONS.each do |hook_name|
+
+        define_method hook_name do |&block|
+          runner.send("register_#{hook_name}_hook", &block)
+        end
+
+      end
+    end
+    private_class_method :define_hooks_on_multiple_transactions
+
     define_hooks_on_single_transactions
-
-    def before_each(&block)
-      runner.register_before_each_hook(&block)
-    end
-
-    def before_each_validation(&block)
-      runner.register_before_each_validation_hook(&block)
-    end
-
-    def after_each(&block)
-      runner.register_after_each_hook(&block)
-    end
-
-    def before_all(&block)
-      runner.register_before_all_hook(&block)
-    end
-
-    def after_all(&block)
-      runner.register_after_all_hook(&block)
-    end
+    define_hooks_on_multiple_transactions
 
     private
 
