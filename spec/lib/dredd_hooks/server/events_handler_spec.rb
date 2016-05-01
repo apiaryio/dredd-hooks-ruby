7696586a63f0a6a0e70b049f62237d2abd11bdec
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2016-05-01 18:50:34 +1000

    Minor fix indentation and case in spec

--- spec/lib/dredd_hooks/server/events_handler_spec.rb
+++ spec/lib/dredd_hooks/server/events_handler_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 module DreddHooks
   describe Server::EventsHandler, private: true do
 
-   let(:events_handler) { described_class.new }
+    let(:events_handler) { described_class.new }
 
-   it { expect(events_handler).to respond_to :handle }
+    it { expect(events_handler).to respond_to :handle }
 
-   describe '#handle' do
+    describe '#handle' do
 
-     it 'returns a transaction' do
-       event = 'do_nothing'
+      it 'returns a transaction' do
+       event = 'doNothing'
        transaction = double()
        expect(events_handler.handle(event, transaction)).to eq transaction
-     end
-   end
+      end
+    end
   end
 end
 
